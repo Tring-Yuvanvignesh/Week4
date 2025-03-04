e209b00cab30061ddb9b32858dab0de575690129
Tring-yuvanvignesh <yuvanvignesh.r@tringapps.com>
2025-03-04 16:03:13 +0530

    commiting changes

--- Task1-Dynamic_Data_Field/src/Components/Item/DisplayData.jsx
+++ Task1-Dynamic_Data_Field/src/Components/Item/DisplayData.jsx
@@ -1,16 +1,39 @@
 import React from 'react'
+import './displayData.css'
+import { data } from 'react-router-dom'
 
 const DisplayData = () => {
 
   const Data = JSON.parse(localStorage.getItem("data")) || []
-  
+
   return (
-    <div className='displayData'>
-        <h2>User Data</h2>
-        <pre>
-            {JSON.stringify(Data, null, 2)}
-        </pre>
-    </div>
+    <>
+      <h2>User Data</h2>
+      <div className='Display'>
+        <div className='displayData'>
+
+          {/* {JSON.stringify(Data, null, 2)} */}
+
+          <table className='data-table'>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Mobile</th>
+            </tr>
+            {
+              Data.map((curr, index) => (
+                <tr key={index}>
+                  <td>{curr.name}</td>
+                  <td>{curr.age}</td>
+                  <td>{curr.mobile}</td>
+                </tr>
+              ))
+            }
+          </table>
+        </div>
+      </div>
+    </>
+
   )
 }
 
